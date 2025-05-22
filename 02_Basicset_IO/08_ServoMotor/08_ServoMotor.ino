a881b34b9c40df5ef496afdc077763de7cd18767
yaetohino <suwajinja2025@gmail.com>
2025-05-22 15:45:31 +0900

    Change angle

--- 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
+++ 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
@@ -9,13 +9,13 @@ void setup() {
 }
 
 void loop() {
-  // 0-180 right
-  for(int angle = 0; angle <= 180; angle++) {                        
+  // 0-45 right
+  for(int angle = 0; angle <= 45; angle++) {                        
     myservo.write(angle);
     delay(15);
   }
-  // 180-0  left 
-  for(int angle = 180; angle >= 0; angle--) {                 
+  // 45-0  left 
+  for(int angle = 45; angle >= 0; angle--) {                 
     myservo.write(angle);  
     delay(15);      
   }
